cc28d382032c9ebf6766557290fece84e47c2408
emilie <emiliefollin@gmail.com>
2019-11-05 10:02:28 +0100

    Ajax - Premier exercice

---
+++ Ajax 1/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ajax 1</title>
+</head>
+<body>
+
+<div id="demo">
+    <h2>Ajax va changer le texte</h2>
+    <button type="button" onclick="GetText()">Ajouter du texte</button>
+</div>
+
+</body>
+</html>
